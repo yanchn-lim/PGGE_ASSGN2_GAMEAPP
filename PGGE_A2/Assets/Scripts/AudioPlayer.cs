6af836d1e5b2bc78c2562557dde06adc7aef27f6
dingledangledongle <105861890+yanchn-lim@users.noreply.github.com>
2024-01-25 05:06:15 +0800

    q3_A

---
+++ PGGE_A2/Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PGGE.Patterns;
+
+public class AudioPlayer : Singleton<AudioPlayer>
+{
+    //audio player inherits from the singleton pattern
+    //so there can only be one audioplayer in the game
+
+    AudioSource source;
+    public AudioClip[] click;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get reference to the audiosource component attached to it
+        source = GetComponent<AudioSource>();
+    }
+
+    //uses PlayOneShot() to only play the sound once
+    public void PlayClickSound(int i)
+    {
+        source.PlayOneShot(click[i]);
+    }
+}
--- PGGE_A2/Assets/Scripts/Menu/Menu.cs
+++ PGGE_A2/Assets/Scripts/Menu/Menu.cs
@@ -5,36 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    public AudioSource source;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void OnClickSinglePlayer()
     {
         //Debug.Log("Loading singleplayer game");
-        source.Play();
         SceneManager.LoadScene("SinglePlayer");
     }
 
     public void OnClickMultiPlayer()
     {
         //Debug.Log("Loading multiplayer game");
-        source.Play();
         SceneManager.LoadScene("Multiplayer_Launcher");
     }
 
     public void OnClickBack()
     {
-        source.Play();
         SceneManager.LoadScene("Menu");
+    }
+
+    public void ClickSound(int i)
+    {
+        AudioPlayer.Instance.PlayClickSound(i);
     }
 }
